92276d0e95d05c77ddb743cab9963a814677c48a
Judith <mulendajudith2018@gmail.com>
2025-05-05 02:05:41 +0000

    pdated the code, to correspomd with the layout

--- app/src/main/java/com/example/moneymate/BudgetActivity.kt
+++ app/src/main/java/com/example/moneymate/BudgetActivity.kt
@@ -1,11 +1,21 @@
 package com.example.moneymate
 
-import androidx.appcompat.app.AppCompatActivity
+
 import android.os.Bundle
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
 
 class BudgetActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_budget)
+
+        // user name
+        val userName = "Franklin Ngangu"
+
+        val welcomeText = findViewById<TextView>(R.id.welcomeText)
+        welcomeText.text = "Welcome, $userName!"
     }
 }
+
+
