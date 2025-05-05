7bcb5ae5eef3de3f02f43f135254d5b7bcabb979
Harris0109 <mumeenaharris69@gmail.com>
2025-05-05 17:33:22 +0200

    Completed the Goals page and connected it to the dashboared (BudgetActivity)

--- app/src/main/java/com/example/moneymate/BudgetActivity.kt
+++ app/src/main/java/com/example/moneymate/BudgetActivity.kt
@@ -16,10 +16,17 @@ class BudgetActivity : AppCompatActivity() {
         val welcomeText = findViewById<TextView>(R.id.welcomeText)
         welcomeText.text = "Welcome, $userName!"
 
-        // Set Button Click to open CreateCategoryActivity
+        // Button to open CreateCategoryActivity
         val btnCreateCategory = findViewById<Button>(R.id.btnCreateCategory)
         btnCreateCategory.setOnClickListener {
             val intent = Intent(this, CreateCategoryActivity::class.java)
+            startActivity(intent)
+        }
+
+        // Button to open GoalsActivity
+        val btnSetMonthlyGoals = findViewById<Button>(R.id.btnSetMonthlyGoals)
+        btnSetMonthlyGoals.setOnClickListener {
+            val intent = Intent(this, GoalsActivity::class.java)
             startActivity(intent)
         }
     }
